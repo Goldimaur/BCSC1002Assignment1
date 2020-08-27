639b43e8bf20b7d59c97e0772950a76ff10b4c6b
Goldi maurya <goldi.maurya_cs19@gla.ac.in>
2020-08-27 21:23:10 +0530

    add a method to set the name of the book

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -19,4 +19,9 @@ public class Book {
     public String getBookName() {
         return bookName;
     }
+
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
 }
