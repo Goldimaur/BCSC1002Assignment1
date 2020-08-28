d8853a7b885ab436012bbcfae4d4b47f83cc5d4c
Goldi maurya <goldi.maurya_cs19@gla.ac.in>
2020-08-28 20:22:36 +0530

    add field to FrontDesk.java .

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -4,10 +4,16 @@
  *  Time: 3:50 PM
  *  File Name : FrontDesk.java
  * */
+
+
 package execution;
 
 public class FrontDesk {
-    public static void main(String[] args) {
+    private static final int ADD_NEW_BOOK = 1;
+    private static final int RETURN_BOOK = 2;
+    private static final int MY_ISSUED_BOOKS = 3;
+    private static final int EXIT = 4;
 
+    public static void main(String[] args) {
     }
 }
